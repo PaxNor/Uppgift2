f9db23d9fca9c984b5cb94c569fbfed41207b9c8
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-05-12 16:13:31 +0200

    Add project files.

---
+++ Uppgift2/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Uppgift 2.
+ */
+
+const string banner =
+@"          --- Main menu ---
+
+Select an option from the alternatives below.
+
+    1) Movie price calculator.
+    2) String manipulation.
+    Q) Quit application.";
+
+
+// main program loop
+while(true)
+{
+    Console.WriteLine(banner);
+    string choice = Console.ReadLine();
+
+    switch(choice)
+    {
+        case "1":
+            break;
+
+        case "2":
+            break;
+
+        case "q":
+        case "Q":
+            Environment.Exit(0);
+            break;
+
+        default:
+            break;
+    }
+
+    Console.Clear();
+}
+
